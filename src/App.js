1410da6ac9fa43c7dab43ca2816a44874591ee9e
Aleksandr <rrewer2018@gmail.com>
2023-02-21 17:04:07 +0100

    added touch events, some displays are supports

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Table from "./components/Numbers/Table";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App- header">2048</header>
+            <div className="App- main">
+                <Table />
+            </div>
+            <footer className="App- footer"></footer>
+        </div>
+    );
+}
+
+export default App;
