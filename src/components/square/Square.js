cb1e933288820ec7fce8743b3ba3d7a3afced554
Aleksandr <rrewer2018@gmail.com>
2023-04-24 14:13:29 +0200

    add Square

---
+++ src/components/square/Square.js
@@ -0,0 +1,18 @@
+import "./Square.css";
+const indent = 25;
+const Square = ({ id, x, y, value, merged, showed }) => (
+    <div
+        key={id}
+        className={`square flex-center ${merged ? "merged" : ""} ${
+            showed ? "showed" : ""
+        } coin${value}`}
+        style={{
+            left: `${x * indent + 1.5}%`,
+            top: `${y * indent + 1.5}%`,
+        }}
+    >
+        <p>{value}</p>
+    </div>
+);
+
+export default Square;
